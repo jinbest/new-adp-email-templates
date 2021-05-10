ba7e43bb634c39e653226d00b7a8f601709710a9
jinbest <jin.bestvictoria718@gmail.com>
2021-05-11 02:17:18 +0800

    dynamic titles for nearest loc, customer info, .svg into .png convertion.

--- app.js
+++ app.js
@@ -12,6 +12,33 @@ Handlebars.registerHelper("equals", function (a, b, opts) {
   else return opts.inverse(this);
 });
 
+Handlebars.registerHelper("ifCond", function (v1, operator, v2, options) {
+  switch (operator) {
+    case "==":
+      return v1 == v2 ? options.fn(this) : options.inverse(this);
+    case "===":
+      return v1 === v2 ? options.fn(this) : options.inverse(this);
+    case "!=":
+      return v1 != v2 ? options.fn(this) : options.inverse(this);
+    case "!==":
+      return v1 !== v2 ? options.fn(this) : options.inverse(this);
+    case "<":
+      return v1 < v2 ? options.fn(this) : options.inverse(this);
+    case "<=":
+      return v1 <= v2 ? options.fn(this) : options.inverse(this);
+    case ">":
+      return v1 > v2 ? options.fn(this) : options.inverse(this);
+    case ">=":
+      return v1 >= v2 ? options.fn(this) : options.inverse(this);
+    case "&&":
+      return v1 && v2 ? options.fn(this) : options.inverse(this);
+    case "||":
+      return v1 || v2 ? options.fn(this) : options.inverse(this);
+    default:
+      return options.inverse(this);
+  }
+});
+
 app.use(bodyParser.json());
 
 app.listen(port, () =>
@@ -60,7 +87,7 @@ function sendEmail(folderName, data) {
     .catch(console.error);
 }
 
-const bananaData = require("./emails/Banana/repair_service_config.json");
+const bananaData = require("./emails/Banana/test.json");
 // const blackappleData = require("./emails/BlackApple/repair_service_config.json")
 // const geeboData = require("./emails/Geebo/repair_service_config.json")
 // const mobileTechData = require("./emails/MobileTech/repair_service_config.json")
