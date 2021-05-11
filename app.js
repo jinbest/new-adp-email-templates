b03f54408da8066e525123315455025067af55f3
jinbest <jin.bestvictoria718@gmail.com>
2021-05-11 16:59:12 +0800

    fixed style issue in outlook, height for table

--- app.js
+++ app.js
@@ -91,11 +91,11 @@ function sendEmail(folderName, data) {
 // const blackappleData = require("./emails/BlackApple/repair_service_config.json");
 // const geeboData = require("./emails/Geebo/repair_service_config.json")
 // const mobileTechData = require("./emails/MobileTech/repair_service_config.json");
-// const nanoTechData = require("./emails/Nanotech/repair_service_config.json")
+const nanoTechData = require("./emails/Nanotech/repair_service_config.json");
 // const northTechData = require("./emails/Northtech/repair_service_config.json")
 // const phonephixData = require("./emails/Phonephix/repair_service_config.json")
 // const reparaData = require("./emails/Reparation/repair_service_config.json")
-const wirelessData = require("./emails/Wireless/repair_service_config.json");
+// const wirelessData = require("./emails/Wireless/repair_service_config.json");
 
 // const dccmtxData = require("./emails/Dccmtx/repair_service_config.json");
 
@@ -104,11 +104,11 @@ app.get("/send-email", function (req, res) {
   // sendEmail("BlackApple", blackappleData);
   // sendEmail('Geebo', geeboData);
   // sendEmail("MobileTech", mobileTechData);
-  // sendEmail('Nanotech', nanoTechData);
+  sendEmail("Nanotech", nanoTechData);
   // sendEmail('Northtech', northTechData);
   // sendEmail('Phonephix', phonephixData);
   // sendEmail('Reparation', reparaData);
-  sendEmail("Wireless", wirelessData);
+  // sendEmail("Wireless", wirelessData);
 
   // sendEmail("Dccmtx", dccmtxData);
 
