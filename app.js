f5067ebcad514bbcae418606915dd2dac2ce9e77
jinbest <jin.bestvictoria718@gmail.com>
2021-05-12 18:40:02 +0800

    Completed MobileTech template

--- app.js
+++ app.js
@@ -92,12 +92,12 @@ function sendEmail(folderName, data) {
 // const bananaData = require("./emails/Banana/repair_service_config.json");
 // const blackappleData = require("./emails/BlackApple/repair_service_config.json");
 // const geeboData = require("./emails/Geebo/repair_service_config.json");
-// const mobileTechData = require("./emails/MobileTech/repair_service_config.json");
+const mobileTechData = require("./emails/MobileTech/repair_service_config.json");
 // const nanoTechData = require("./emails/Nanotech/repair_service_config.json");
 // const northTechData = require("./emails/Northtech/repair_service_config.json");
 // const phonephixData = require("./emails/Phonephix/repair_service_config.json");
 // const reparaData = require("./emails/Reparation/repair_service_config.json");
-const wirelessData = require("./emails/Wireless/repair_service_config.json");
+// const wirelessData = require("./emails/Wireless/repair_service_config.json");
 
 // const dccmtxData = require("./emails/Dccmtx/repair_service_config.json");
 
@@ -105,12 +105,12 @@ app.get("/send-email", function (req, res) {
   // sendEmail("Banana", bananaData);
   // sendEmail("BlackApple", blackappleData);
   // sendEmail("Geebo", geeboData);
-  // sendEmail("MobileTech", mobileTechData);
+  sendEmail("MobileTech", mobileTechData);
   // sendEmail("Nanotech", nanoTechData);
   // sendEmail("Northtech", northTechData);
   // sendEmail("Phonephix", phonephixData);
   // sendEmail("Reparation", reparaData);
-  sendEmail("Wireless", wirelessData);
+  // sendEmail("Wireless", wirelessData);
 
   // sendEmail("Dccmtx", dccmtxData);
 
