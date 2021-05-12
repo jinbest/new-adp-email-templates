c3322116ab1960cfc74d53bb5e9ed75a80fb31e2
jinbest <jin.bestvictoria718@gmail.com>
2021-05-13 00:41:57 +0800

    Make bounce div as clickable link for all templates

--- app.js
+++ app.js
@@ -92,8 +92,8 @@ function sendEmail(folderName, data) {
 // const bananaData = require("./emails/Banana/repair_service_config.json");
 // const blackappleData = require("./emails/BlackApple/repair_service_config.json");
 // const geeboData = require("./emails/Geebo/repair_service_config.json");
-const mobileTechData = require("./emails/MobileTech/repair_service_config.json");
-// const nanoTechData = require("./emails/Nanotech/repair_service_config.json");
+// const mobileTechData = require("./emails/MobileTech/repair_service_config.json");
+const nanoTechData = require("./emails/Nanotech/repair_service_config.json");
 // const northTechData = require("./emails/Northtech/repair_service_config.json");
 // const phonephixData = require("./emails/Phonephix/repair_service_config.json");
 // const reparaData = require("./emails/Reparation/repair_service_config.json");
@@ -105,8 +105,8 @@ app.get("/send-email", function (req, res) {
   // sendEmail("Banana", bananaData);
   // sendEmail("BlackApple", blackappleData);
   // sendEmail("Geebo", geeboData);
-  sendEmail("MobileTech", mobileTechData);
-  // sendEmail("Nanotech", nanoTechData);
+  // sendEmail("MobileTech", mobileTechData);
+  sendEmail("Nanotech", nanoTechData);
   // sendEmail("Northtech", northTechData);
   // sendEmail("Phonephix", phonephixData);
   // sendEmail("Reparation", reparaData);
